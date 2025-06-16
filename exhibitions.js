8d2a9a50b4df7d9485ea70adbd8201e1b4cdc476
Soel-c <sulthonfabian21@gmail.com>
2025-06-16 15:31:56 +0700

    final

--- exhibitions.js
+++ exhibitions.js
@@ -8,7 +8,7 @@ const EventCard = ({ title, description, imageSrc, alt }) => {
             <div className="event-info">
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <a href="traveloka.com" className="book-online">Pesan Online</a>
+                <a href="https://www.traveloka.com" className="book-online">Pesan Online</a>
             </div>
         </div>
     );
